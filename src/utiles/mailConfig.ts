9089e86abcee40597910841e560bdc63075ed706
Mian Muhammad Anees <miananees23@gmail.com>
2025-02-13 11:37:30 +0500

    fix:(emails) send emails for resend

--- src/utiles/mailConfig.ts
+++ src/utiles/mailConfig.ts
@@ -1,15 +1,25 @@
-import nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 import dotenv from 'dotenv';
-dotenv.config();
-console.log(process.env.SES_SMTP_USERNAME,
-    process.env.SES_SMTP_PASSWORD, "==== in env")
 
-export const transporter = nodemailer.createTransport({
-    host: 'email-smtp.us-east-1.amazonaws.com',
-    port: 587,
-    secure: false,  // True for 465, false for other ports
-    auth: {
-        user: process.env.SES_SMTP_USERNAME,
-        pass: process.env.SES_SMTP_PASSWORD
+dotenv.config();
+
+console.log(process.env.RESEND_API_KEY, "==== in env");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async ({ to, subject, html }) => {
+    try {
+        const response = await resend.emails.send({
+            from: 'no-reply@scholarbee.pk', // Update with your verified sender email
+            to,
+            subject,
+            html
+        });
+
+        console.log('Email sent:', response);
+        return response;
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw error;
     }
-});
+};
